70088f372c1057a5a73c1b8b1f8c69b1be5d7d36
LeotendoDev <leotendosteinhoff@gmail.com>
2022-03-12 10:28:31 +0100

    Initalize commit!

---
+++ Binary/CMakeFiles/frost_engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/frost_engine.dir/Core/Entry.cpp.o"
+  "CMakeFiles/frost_engine.dir/Core/Entry.cpp.o.d"
+  "frost_engine"
+  "frost_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/frost_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ -g -std=c++20 Core/Entry.cpp -o Binary/frost_engine -lGL -lSDL2 -lGLEW
